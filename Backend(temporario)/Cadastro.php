705da3e7263d54cd9570102d08ed8ebee967a538
Awiwa <murilodebiazi@gmail.com>
2025-09-13 02:48:30 -0300

    Tentei atualizar o cadastro pra usar classe Usuario

--- Backend(temporario)/Cadastro.php
+++ Backend(temporario)/Cadastro.php
@@ -14,12 +14,13 @@ if ($conn->connect_error) {
 
 // prepare and bind
 $stmt = $conn->prepare("INSERT INTO Usuario (nomeUsuario, emailUsuario, senhaUsuario) VALUES (?, ?, ?)");
-$stmt->bind_param("sss", $nome, $email, $senha);
+$stmt->bind_param("sss", $usuario->getNome, $usuario->getEmail, $usuario->getSenha);
 
+$usuario = new Usuario();
 // set parameters and execute
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$senha = $_POST['password'];
+$usuario->setNome($_POST['nome']);
+$usuario->setEmail($_POST['email']);
+$usuario->setSenha($_POST['password'])
 
 $stmt->execute();
 
@@ -28,3 +29,4 @@ echo "New records created successfully";
 $stmt->close();
 $conn->close();
 ?>
+
