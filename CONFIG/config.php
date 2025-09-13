720af8bfd94f4defa77667c84f57533f4867e3d4
Awiwa <murilodebiazi@gmail.com>
2025-09-13 09:48:44 -0300

    Update config.php

--- CONFIG/config.php
+++ CONFIG/config.php
@@ -1,12 +1,12 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "LORY";
+    $host = "localhost";
+    user = "root";
+    $pass = "root";
+    $db   = "LORY";
 
-$conn = new mysqli($host, $user, $pass, $db);
+    $conn = new mysqli($host, $user, $pass, $db);
 
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
-}
+    if ($conn->connect_error) {
+        die("Falha na conexão: " . $conn->connect_error);
+    }
 ?>
